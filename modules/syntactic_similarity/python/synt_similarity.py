6e2d891e992de3aa55b322f9da75e276c7ab0a51
AkhilHector <akhilhector.1@gmail.com>
2017-09-15 16:46:43 -0500

    Changed directory names for modules and touch syntactic similarity module
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

---
+++ modules/syntactic_similarity/python/synt_similarity.py
@@ -0,0 +1,16 @@
+# This Source Code Form is subject to the terms of the MPL
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/AkhilHector/pubundsci/blob/master/LICENSE.
+
+import sys
+import nltk
+import string
+
+def syntax_similarity_score(sentence_one, sentence_two):
+    return sentence_one, sentence_two
+
+if __name__ == "__main__":
+    print syntax_similarity_score(sentence_a, sentence_b)
+else:
+    sys.exit(0)
