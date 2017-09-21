6157d5823e1056e683b409e6d527160a9f9db123
AkhilHector <akhilhector.1@gmail.com>
2017-09-20 19:13:36 -0500

    Added a parse tree construction function for understanding syntactic similarity of a sentence
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- modules/syntactic_similarity/python/synt_similarity.py
+++ modules/syntactic_similarity/python/synt_similarity.py
@@ -5,12 +5,35 @@
 
 import sys
 import nltk
-import string
+from nltk import pos_tag as postag
+from nltk import word_tokenize as tokenize
+from nltk import RegexpParser as parser
+
+def draw_parse_tree(sentence):
+    # init a list and store the modified as a list item
+    mod_sentence = []
+    mod_sentence.append(sentence)
+
+    # iterate over the list and create a parse tree of the sentence
+    for words in mod_sentence:
+        # tokenize the sentence and attach pos tags to each word
+        tokens = tokenize(words)
+        tagged_words = postag(tokens)
+
+        # use regexpparser of nltk to parse the pos tagged words and store in a nltk tree list
+        chunk = r"""Chunk: {<RB.?>*<VB.?>*<NNP>}"""
+        form_parsed_chunk = parser(chunk)
+        tree = form_parsed_chunk.parse(tagged_words)
+        tree.draw()
+        return tree
 
 def syntax_similarity_score(sentence_one, sentence_two):
     return sentence_one, sentence_two
 
 if __name__ == "__main__":
-    print syntax_similarity_score(sentence_a, sentence_b)
+    sentence_a = "Woman, without her man, is helpless.";
+    sentence_b = "Woman! Without her, man is helpless!"
+    # print syntax_similarity_score(sentence_a, sentence_b)
+    print draw_parse_tree(sentence_b)
 else:
     sys.exit(0)
