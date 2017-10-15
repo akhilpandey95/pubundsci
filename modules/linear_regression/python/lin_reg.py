382b853785033ab38b9b57d1a7d3341a11d2bd09
AkhilHector <akhilhector.1@gmail.com>
2017-10-15 18:12:14 -0500

    Init Linear Regression Model
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

---
+++ modules/linear_regression/python/lin_reg.py
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the MPL
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/AkhilHector/pubundsci/blob/master/LICENSE.
+
+import sys
+
+if __name__ == "__main__":
+    print "Linear Regression Model"
+else:
+    sys.exit(0)
