ca5be91de2ac29347038f4bf4a3b97a5d35d9ed6
AkhilHector <akhilhector.1@gmail.com>
2017-11-14 17:11:46 -0600

    Removed spaces from bigram module and updated the result for trigram module
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- modules/lexical_features/python/lex_features.py
+++ modules/lexical_features/python/lex_features.py
@@ -59,28 +59,25 @@ def compute_collocation_score(sentence_one, sentence_two, option):
     if option == "bi":
         tokens_for_one = nltk.wordpunct_tokenize(sentence_one)
         tokens_for_two = nltk.wordpunct_tokenize(sentence_two)
-
         finder_one = BigramCollocationFinder.from_words(tokens_for_one)
         finder_two = BigramCollocationFinder.from_words(tokens_for_two)
-
         result_one = finder_one.score_ngrams(nltk.collocations.BigramAssocMeasures().raw_freq)
-
         result_one = [(tuple(map(str.lower, values)), scores) for values, scores in result_one]
-
         result_two = finder_two.score_ngrams(nltk.collocations.BigramAssocMeasures().raw_freq)
-
         result_two = [(tuple(map(str.lower, values)), scores) for values, scores in result_two]
-
-
         matches = [keys for keys in set(result_one).intersection(set(result_two))]
-        return result_one
+        return len(matches)
     elif option == "tri":
         tokens_for_one = nltk.wordpunct_tokenize(sentence_one)
         tokens_for_two = nltk.wordpunct_tokenize(sentence_two)
         finder_one = TrigramCollocationFinder.from_words(tokens_for_one)
         finder_two = TrigramCollocationFinder.from_words(tokens_for_two)
-        result = finder_one.score_ngrams(nltk.collocations.TrigramAssocMeasures().raw_freq)
-        return result
+        result_one = finder_one.score_ngrams(nltk.collocations.TrigramAssocMeasures().raw_freq)
+        result_one = [(tuple(map(str.lower, values)), scores) for values, scores in result_one]
+        result_two = finder_two.score_ngrams(nltk.collocations.TrigramAssocMeasures().raw_freq)
+        result_two = [(tuple(map(str.lower, values)), scores) for values, scores in result_two]
+        matches = [keys for keys in set(result_one).intersection(set(result_two))]
+        return len(matches)
     else:
         return 0
 
@@ -89,6 +86,6 @@ if __name__ == "__main__":
     a = "Although population-level genomic sequence data have been gathered extensively for humans similar data from our closest living relatives are just beginning to emerge. Examination of genomic variation within great apes offers many opportunities to increase our understanding of the forces that have differentially shaped the evolutionary history of hominid taxa. Here we expand upon the work of the Great Ape Genome Project by analyzing medium to high coverage whole-genome sequences from 14 western lowland gorillas (Gorilla gorilla gorilla) 2 eastern lowland gorillas (G. beringei graueri) and a single Cross River individual (G. gorilla diehli). We infer that the ancestors of western and eastern lowland gorillas diverged from a common ancestor approximately 261 ka and that the ancestors of the Cross River population diverged from the western lowland gorilla lineage approximately 68 ka. Using a diffusion approximation approach to model the genome-wide site frequency spectrum we infer a history of western lowland gorillas that includes an ancestral population expansion of 1.4-fold around 970 ka and a recent 5.6-fold contraction in population size 23 ka. The latter may correspond to a major reduction in African equatorial forests around the Last Glacial Maximum. We also analyze patterns of variation among western lowland gorillas to identify several genomic regions with strong signatures of recent selective sweeps. We find that processes related to taste pancreatic and saliva secretion sodium ion transmembrane transport and cardiac muscle function are overrepresented in genomic regions predicted to have experienced recent positive selection."
     b = "Please note: this list is intended as a resource for those of you who are interested in responding to other CEHG members&rsquo; works for the blog. Let the blog editor (kkanagaw@stanford.edu) know if there are other publications that should be added to the"
     print compute_yules_i_for_text(s)
-    print compute_collocation_score(a, b, "bi")
+    print compute_collocation_score(a, b, "tri")
 else:
     sys.exit(0)
